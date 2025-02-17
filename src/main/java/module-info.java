6f205daddcab6c3cf9cd6a080debb952f8f7bedc
Glayson.Santos <glaysonbrennod.santos@gmail.com>
2025-02-17 20:54:27 -0300

    Local project created

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module application.workshopjavafxjdbc {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens application.workshopjavafxjdbc to javafx.fxml;
+    exports application.workshopjavafxjdbc;
+}
